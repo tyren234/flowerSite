9d2dbec3bb0b25d8dc5d148fc415cca39b17ee5f
Tymon <tyren234@gmail.com>
2025-09-14 21:57:31 +0200

    Updated website

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # flowerSite
 Personal react website
 
+# Develop
+In `./flower-site-ui` run
+```bash
+npm i
+```
+To run frontend run
+```
+npm run start
+```
+
+
+# Update
 To update run 
 
-```npm run build```
+```bash
+npm run build
+```
 
 in `./flower-site-ui` and copy new files to `./`
--- flower-site-ui/src/pages/HomePage/HomePage.tsx
+++ flower-site-ui/src/pages/HomePage/HomePage.tsx
@@ -38,7 +38,7 @@ function HomePage() {
                                 Back in the middle school, I picked up coding and slowly built up confidence with mostly
                                 C++. In senior year of high school and first year of university, I started really
                                 focusing on programming in C++ and Python. It was also then that I finally decided to
-                                install Ubuntu on my main PC (I use dual boot).
+                                install Ubuntu on my main PC.
                             </p>
                         </section>
                     </div>
@@ -47,15 +47,35 @@ function HomePage() {
                     <div className="card-content">
                         <section>
                             <p>
-                                Now I study Computer Science (Geoinformatics) at Warsaw University of Technology, give
-                                tutoring in high school mathematics and look for a junior developer position.
+                                I studied Computer Science (Geoinformatics) at Warsaw University of Technology. Now I work as a software developer and I look forward to getting a master degree in computer science.
                             </p>
                             <p>
-                                I hope you'll find this site entertaining and worth your time :). (I really
+                                I hope you'll find this site entertaining and you learn something about me :). (I really
                                 recommend <Link className={styles.underlineLink} to={"/memes"}>memes</Link> tab)
                             </p>
                         </section>
                     </div>
+                </div>
+                <div className="card left">
+                    <div className="card-content">
+                    <section>
+                            <p>Some of my projects</p>
+                            <ul style={{textAlign: "left", lineHeight: "1.5rem"}}>
+                                <li>
+                                    <Link className={styles.underlineLink}  to={"https://github.com/tyren234/sunflower"} target={"_blank"}>Sunflower</Link> -
+                                    a discord bot that lets you save your message to local filesystem. I run this on my home server to make sure my notes are backed up.
+                                </li> 
+                                <li>
+                                    <Link className={styles.underlineLink} to={"https://github.com/tyren234/satellites-position-visualiser"} target={"_blank"}>Satellite position visualiser</Link> -
+                                    visualisation app showing you precise locations of multiple satellites from many different satellite systems.
+                                </li>
+                                <li>
+                                    <Link className={styles.underlineLink} to={"https://github.com/tyren234/fantasy-leaflet-map"} target={"_blank"}>Fantasy world map</Link> -
+                                    interactive web map I made for my D&D campaign. You can see it <Link className={styles.underlineLink} to={"https://tyren234.github.io/fantasy-leaflet-map/"} target={"_blank"}>here</Link>.
+                                </li>
+                            </ul>
+                    </section>
+                </div>
                 </div>
                 <div className="card">
                     <div className="card-content">
@@ -64,7 +84,7 @@ function HomePage() {
                                 My interests:
                             </p>
                             <ul style={{textAlign: "left", lineHeight: "1.5rem"}}>
-                                <li>Java and Design Patterns (I developed in Java while working in Goldman Sachs)</li>
+                                <li>Java and Design Patterns (I use Java at work)</li>
                                 <li>C++ (I started here, it was my primary language at the university. I took part in
                                     Nokia Academy for C++ developers in autumn 2022)
                                 </li>
